1f493118f4370d3238b40d28d4e75ec8ad40fe08
diganta15 <ddas461598@gmail.com>
2021-12-15 17:59:35 +0530

    Create profile schema and current user details route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,29 @@
-const express = require('express');
+const express = require("express");
+const auth = require("../../middleware/auth");
+
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
 const router = express.Router();
 
+//@route    POST api/profile/me
+//@desc    Get current user's profile
+//@access   Private
+router.get("/me", auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({ user: req.user.id }).populate(
+			"user",
+			["name", "avatar"]
+		);
+		if (!profile) {
+			return res.status(400).json({ msg: "There is no profile for this user" });
+		}
 
-//@route    POST api/profile
-//@desec   Login user
-//@access   Public
-router.get('/', (req, res) => {
-    res.send('Profile route');
+		res.json(profile);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send("Internal Server Error");
+	}
 });
 
 module.exports = router;
